8384f1da0a81c25ff2486d2ab26a26289875a44f
GOOMBANGEE <goom@goomba.org>
2025-01-12 09:07:59 +0900

    update: done check, delete button in detail modal

--- src/component/TodoDetail.tsx
+++ src/component/TodoDetail.tsx
@@ -4,10 +4,12 @@ import { TodoState } from "../..";
 import { useGlobalStore } from "../GlobalStore";
 import { useTodoStore } from "../TodoStore";
 import useDebounce from "../hook/useDebounce";
+import useTodoDelete from "../hook/useTodoDelete";
 import useTodoUpdate from "../hook/useTodoUpdate";
 
 export default function TodoDetail() {
   const { todoUpdate } = useTodoUpdate();
+  const { todoDelete } = useTodoDelete();
   const { todoState, setTodoState, todoListState, setTodoListState } =
     useTodoStore();
   const { globalState, setGlobalState } = useGlobalStore();
@@ -88,7 +90,7 @@ export default function TodoDetail() {
       <div className="todo-detail-modal bg-customDark_6 z-10 rounded px-6 py-4">
         <ul className="flex w-full flex-col gap-y-2">
           {/* title */}
-          <li className="mb-2">
+          <li className="mb-2 flex">
             {updateState.title ? (
               <input
                 defaultValue={todoState.title}
@@ -117,7 +119,59 @@ export default function TodoDetail() {
                 {todoState.title}
               </button>
             )}
+
+            <div className="flex gap-x-2">
+              {/* done check */}
+              <button
+                className={`relative flex h-6 w-6 flex-shrink-0 cursor-pointer items-center rounded ${todoState.isDone ? "bg-green-600" : "bg-white"}`}
+                onClick={() => {
+                  todoUpdate({
+                    todo: {
+                      ...todoState,
+                      isDone: !todoState.isDone,
+                    },
+                  });
+                }}
+              >
+                <span
+                  className={`absolute left-1.5 text-white ${todoState.isDone ? "block" : "hidden"}`}
+                >
+                  &#10003;
+                </span>
+              </button>
+              {/* delete */}
+              <button
+                onClick={() => {
+                  todoState.id ? todoDelete({ id: todoState.id }) : null;
+                }}
+              >
+                <svg
+                  width="24px"
+                  height="24px"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  stroke="#EBEBEB"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path
+                      d="M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6M18 6V16.2C18 17.8802 18 18.7202 17.673 19.362C17.3854 19.9265 16.9265 20.3854 16.362 20.673C15.7202 21 14.8802 21 13.2 21H10.8C9.11984 21 8.27976 21 7.63803 20.673C7.07354 20.3854 6.6146 19.9265 6.32698 19.362C6 18.7202 6 17.8802 6 16.2V6M14 10V17M10 10V17"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                  </g>
+                </svg>
+              </button>
+            </div>
           </li>
+
           {/* description */}
           <li className="mb-2">
             {updateState.description ? (
@@ -202,24 +256,6 @@ export default function TodoDetail() {
             </div>
           </li>
 
-          {/* done check */}
-          {/* <button
-              className={`relative flex h-6 w-6 flex-shrink-0 cursor-pointer items-center rounded ${todoState.isDone ? "bg-green-600" : "bg-white"}`}
-              onClick={() => {
-                todoUpdate({
-                  todo: {
-                    ...todoState,
-                    isDone: !todoState.isDone,
-                  },
-                });
-              }}
-            >
-              <span
-                className={`absolute left-1.5 text-white ${todoState.isDone ? "block" : "hidden"}`}
-              >
-                &#10003;
-              </span>
-            </button> */}
           <li className="flex justify-end gap-x-2"></li>
         </ul>
       </div>
